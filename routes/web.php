f16b00e5f73a7514bc93768cd638e26cd894536d
Fahril Refiandi <fahril.refandi@gmail.com>
2022-03-08 12:36:39 +0700

    main

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::resource('/barang',BarangController::class);
